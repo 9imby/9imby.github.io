c6076ad0efc11f0983ee35e7e6d190db13c4763c
FelineNibbler <57281752+FelineNibbler@users.noreply.github.com>
2019-11-09 22:26:55 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -8,4 +8,13 @@ questions = [
             "scty": 0
         }
     },
+    {
+        "question": "The government should provide free housing to those who cannot afford it.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
 ];
