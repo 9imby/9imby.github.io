b4b0bfb2235fc655c92dad13bafa933e4d11c207
FelineNibbler <57281752+FelineNibbler@users.noreply.github.com>
2019-11-09 22:30:47 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -17,4 +17,31 @@ questions = [
             "scty": 0
         }
     },
+    {
+        "question": "I support Safe Injection Sites in my neighbourhood.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I support new high-density buildings in my neighbourhood.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I support new low-density buildings in my neighbourhood.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
 ];
